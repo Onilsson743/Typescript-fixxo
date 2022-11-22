20480458370fd0ec10d93087d70d3c50c6a71265
Onilsson743 <O.nilsson743@gmail.com>
2022-11-22 08:52:26 +0100

    working on fetching products

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,22 @@
+import { useEffect } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Contact from './pages/Contact';
 import Home from './pages/Home';
 
 
 function App() {
+
+  async function FetchProducts() {
+    // const response = await fetch('http://localhost:5000/products')
+    // console.log(response)
+    // setProducts({...products, all: await response.json()})
+  }
+  
+
+  useEffect(() => {
+    FetchProducts()
+  }, [])
+
   return (
     <div >
       <BrowserRouter>
